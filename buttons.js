eb2b74b65bc96a14651621ab94476907da3eeb0a
Eric Abell <ericabell@users.noreply.github.com>
2017-08-03 16:59:04 -0500

    Starting to make 15C keyboard buttons

---
+++ buttons.js
@@ -0,0 +1,148 @@
+let buttonGridLeft = `
+  <div class='row' id='row-1'>
+
+    <div class='button' id='sqrt-button'>
+      <div class='shifted-alt-function' id='A'>
+        A
+      </div>
+      <div class='primary-function' id='sqrt'>
+        $\\sqrt{x}$
+      </div>
+      <div class='alt-function' id='square'>
+        $x^2$
+      </div>
+    </div>
+
+    <div class='button' id='e-to-x-button'>
+      <div class='shifted-alt-function' id='B'>
+        B
+      </div>
+      <div class='primary-function' id='e-to-x'>
+        $e^x$
+      </div>
+      <div class='alt-function' id='ln'>
+        $\\ln$
+      </div>
+    </div>
+
+    <div class='button' id='10-to-x-button'>
+      <div class='shifted-alt-function' id='C'>
+        C
+      </div>
+      <div class='primary-function' id='10-to-x'>
+        $10^x$
+      </div>
+      <div class='alt-function' id='log'>
+        $\\log$
+      </div>
+    </div>
+
+    <div class='button' id='y-to-x-button'>
+      <div class='shifted-alt-function' id='D'>
+        D
+      </div>
+      <div class='primary-function' id='y-to-x'>
+        $y^x$
+      </div>
+      <div class='alt-function' id='percent'>
+        $%$
+      </div>
+    </div>
+
+    <div class='button' id='reciprocal-button'>
+      <div class='shifted-alt-function' id='E'>
+        E
+      </div>
+      <div class='primary-function' id='reciprocal'>
+        $1/x$
+      </div>
+      <div class='alt-function' id='delta-percent'>
+        $\\delta %$
+      </div>
+    </div>
+
+  </div>
+
+  <div class='row' id='row-2'>
+
+    <div class='button' id='sst-button'>
+      <div class='shifted-alt-function' id='lbl'>
+        LBL
+      </div>
+      <div class='primary-function' id='sst'>
+        SST
+      </div>
+      <div class='alt-function' id='bst'>
+        $BST$
+      </div>
+    </div>
+
+    <div class='button' id='gto-button'>
+      <div class='shifted-alt-function' id='hyp'>
+        HYP
+      </div>
+      <div class='primary-function' id='gto'>
+        GTO
+      </div>
+      <div class='alt-function' id='hyp-inverse'>
+        $HYP^{-1}$
+      </div>
+    </div>
+
+    <div class='button' id='sin-button'>
+      <div class='shifted-alt-function' id='dim'>
+        DIM
+      </div>
+      <div class='primary-function' id='sin'>
+        $\\sin$
+      </div>
+      <div class='alt-function' id='arcsin'>
+        $\\arcsin$
+      </div>
+    </div>
+
+    <div class='button' id='cos-button'>
+      <div class='shifted-alt-function' id='omega'>
+        $\\omega$
+      </div>
+      <div class='primary-function' id='cos'>
+        $\\cos$
+      </div>
+      <div class='alt-function' id='arccos'>
+        $\\arccos$
+      </div>
+    </div>
+
+    <div class='button' id='tan-button'>
+      <div class='shifted-alt-function' id='I'>
+        I
+      </div>
+      <div class='primary-function' id='tan'>
+        $\\tan$
+      </div>
+      <div class='alt-function' id='arctan'>
+        $\\arctan$
+      </div>
+    </div>
+
+
+
+  </div>
+
+  <div class='row' id='row-3'>
+
+  </div>
+
+  <div class='row' id='row-4'>
+
+  </div>
+
+`
+
+let buttonGridCenter = `
+
+`
+
+let buttonGridRight = `
+
+`
